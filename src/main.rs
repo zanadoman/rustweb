2db27f5c294275783863438be9cab3515c59fc2d
zanadoman <domanseven@gmail.com>
2024-10-24 12:30:15 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -4,36 +4,53 @@ mod routes;
 mod services;
 mod templates;
 
-use std::env::var;
+use std::{env::var, error::Error};
 
-use axum::serve;
+use axum::{extract::Request, serve};
 use dotenv::dotenv;
 use services::authenticator::AuthenticatorService;
 use sqlx::MySqlPool;
-use tokio::{main, net::TcpListener};
-use tracing_subscriber::{fmt, fmt::format::FmtSpan};
+use tokio::{main, net::TcpListener, signal::ctrl_c};
+use tower_http::trace::TraceLayer;
+use tracing::{info, span, Level};
+use tracing_subscriber::{
+    fmt::{self, format::FmtSpan},
+    layer::SubscriberExt,
+    registry,
+    util::SubscriberInitExt,
+    EnvFilter,
+};
 
 use routes::routes;
 
 #[main]
-async fn main() {
-    dotenv().expect(".env missing");
-
-    fmt()
-        .with_span_events(FmtSpan::FULL)
-        .with_target(false)
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv()?;
+    registry()
+        .with(EnvFilter::try_from_default_env()?)
+        .with(fmt::layer().with_span_events(FmtSpan::NEW | FmtSpan::CLOSE))
         .init();
-
-    let database = MySqlPool::connect(var("DATABASE_URL").unwrap().as_str())
-        .await
-        .unwrap();
-
+    let listener = TcpListener::bind(var("APP_ADDRESS")?.as_str()).await?;
+    info!("{:?}", listener);
+    let database = MySqlPool::connect(var("DATABASE_URL")?.as_str()).await?;
+    info!("{:?}", database);
     serve(
-        TcpListener::bind("0.0.0.0:8000").await.unwrap(),
+        listener,
         routes()
-            .layer(AuthenticatorService::new(&database).await.unwrap())
+            .layer(TraceLayer::new_for_http().make_span_with(
+                |request: &Request| {
+                    span! {
+                        Level::DEBUG,
+                        "request",
+                        method = ?request.method(),
+                        route = ?request.uri(),
+                    }
+                },
+            ))
+            .layer(AuthenticatorService::new(&database).await?)
             .with_state(database.into()),
     )
-    .await
-    .unwrap();
+    .with_graceful_shutdown(async { ctrl_c().await.unwrap() })
+    .await?;
+    Ok(())
 }
--- src/services/authenticator.rs
+++ src/services/authenticator.rs
@@ -1,12 +1,11 @@
 use axum::async_trait;
 use axum_login::{
-    tower_sessions::{Expiry, SessionManagerLayer},
+    tower_sessions::{Expiry, MemoryStore, SessionManagerLayer},
     AuthManagerLayer, AuthManagerLayerBuilder, AuthnBackend, UserId,
 };
 use password_auth::verify_password;
 use sqlx::{Error, MySqlPool};
 use time::Duration;
-use tower_sessions_sqlx_store::MySqlStore;
 
 use crate::models::user::UserModel;
 
@@ -39,14 +38,13 @@ impl AuthnBackend for AuthenticatorService {
 impl AuthenticatorService {
     pub async fn new(
         database: &MySqlPool,
-    ) -> Result<AuthManagerLayer<AuthenticatorService, MySqlStore>, Error> {
-        let storage = MySqlStore::new(database.clone());
-        storage.migrate().await?;
+    ) -> Result<AuthManagerLayer<AuthenticatorService, MemoryStore>, Error>
+    {
         Ok(AuthManagerLayerBuilder::new(
             AuthenticatorService {
                 0: database.clone(),
             },
-            SessionManagerLayer::new(storage)
+            SessionManagerLayer::new(MemoryStore::default())
                 .with_expiry(Expiry::OnInactivity(Duration::minutes(10)))
                 .with_secure(false),
         )
