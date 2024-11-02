98ca7e00b5330b365b85df5beefefbc71a39cc28
zanadoman <domanseven@gmail.com>
2024-11-02 16:30:51 +0100

    .

--- src/templates/authentication.rs
+++ src/templates/authentication.rs
@@ -1,7 +1,7 @@
 use askama::Template;
 
 #[derive(Template)]
-#[template(path = "authentication.html")]
+#[template(path = "./authentication.html")]
 pub struct AuthenticationTemplate<'a> {
     pub token: &'a str,
     pub location: &'a str,
--- src/templates/dashboard.rs
+++ src/templates/dashboard.rs
@@ -1,7 +1,7 @@
 use askama::Template;
 
 #[derive(Template)]
-#[template(path = "dashboard.html")]
+#[template(path = "./dashboard.html")]
 pub struct DashboardTemplate<'a> {
     pub token: &'a str,
     pub location: &'a str,
