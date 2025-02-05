b94ba9b1399c2262dbf2437337914bcf3459a637
zanadoman <domanseven@gmail.com>
2025-02-05 15:36:45 +0100

    added missing async-trait crate

--- src/services/authenticator.rs
+++ src/services/authenticator.rs
@@ -1,4 +1,4 @@
-use axum::async_trait;
+use async_trait::async_trait;
 use axum_login::{
     tower_sessions::{Expiry, MemoryStore, SessionManagerLayer},
     AuthManagerLayer, AuthManagerLayerBuilder, AuthnBackend, UserId,
